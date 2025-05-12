0aaac2bc70e0dedac5313a8c2b3dfa0e132f3188
PreethamMR22 <preethamarivu22@gmail.com>
2025-05-12 23:07:35 +0530

    added onboarding, for both db and for streamer, and updated the middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,12 @@ import express from "express"
 import "dotenv/config";
 import authRoutes from './routes/auth.routes.js'
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser"
 const app= express()
 
 
 const PORT= process.env.PORT
-
+app.use(cookieParser()); 
 app.use(express.json());
 app.use("/api/auth",authRoutes)
 
